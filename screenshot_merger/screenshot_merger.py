f00d45b6757381d362b81594ad3da40abe461d3a
emi <esauvisky@gmail.com>
2024-10-01 04:34:39 -0300

    refactor: setup logging infrastructure

--- screenshot_merger/screenshot_merger.py
+++ screenshot_merger/screenshot_merger.py
@@ -11,6 +11,40 @@ import platform
 import subprocess
 from io import BytesIO
 import os
+from loguru import logger
+import tkinter as tk  # Added for displaying images
+
+def setup_logging(log_lvl="DEBUG", options={}):
+    file = options.get("file", False)
+    function = options.get("function", False)
+    process = options.get("process", False)
+    thread = options.get("thread", False)
+
+    log_fmt = (u"<n><d><level>{time:HH:mm:ss.SSS} | " +
+               f"{'{file:>15.15}' if file else ''}" +
+               f"{'{function:>15.15}' if function else ''}" +
+               f"{':{line:<4} | ' if file or function else ''}" +
+               f"{'{process.name:>12.12} | ' if process else ''}" +
+               f"{'{thread.name:<11.11} | ' if thread else ''}" +
+               u"{level:1.1} | </level></d></n><level>{message}</level>")
+
+    logger.configure(
+        handlers=[{
+            "sink": lambda x: print(x, end=""),
+            "level": log_lvl,
+            "format": log_fmt,
+            "colorize": True,
+            "backtrace": True,
+            "diagnose": True
+        }],
+        levels=[
+            {"name": "TRACE", "color": "<white><dim>"},
+            {"name": "DEBUG", "color": "<cyan><dim>"},
+            {"name": "INFO", "color": "<white>"}
+        ]
+    )  # type: ignore # yapf: disable
+
+setup_logging("DEBUG", {"function": True, "thread": True})
 
 # Check Operating System
 OS_NAME = platform.system()
